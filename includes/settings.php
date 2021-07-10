5840f2fff050c5780103048d52d61044a9fcfee8
BB2Xd <74978198+BB2Xd@users.noreply.github.com>
2021-07-11 03:58:53 +0530

    Update settings.php

--- includes/settings.php
+++ includes/settings.php
@@ -9,14 +9,14 @@
 #/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
 /*  Telegram Bot API Key */
-define('API_KEY','1885614923:AAFsK7TQYsIMGVaRNxue9l70JasGQl06Cio');  # Enter bot api token
+define('API_KEY','1885614923:AAG7UBZaJlQ8eBrYSRB8BOXeW3hJVEMR-CI');  # Enter bot api token
 define('APP_URL','https://api.telegram.org/bot'.API_KEY);   ## Don't edit this line ##
 
 #===============================================================================
 #-------------------------- Connect to database --------------------------------
 #===============================================================================
 try {
-    $conn = new PDO('mysql:host=containers-us-west-9.railway.app;dbname=railway;port=6654;charset=utf8mb4', root, EnRrzLwKSPXOscxplNY2);
+    $conn = new PDO('mysql:host=containers-us-west-9.railway.app;dbname=railway;port=7143;charset=utf8mb4', root, Hje662ftRASgSw6pbGu4);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //Create tables
     include_once __DIR__ . '/tables.php';
